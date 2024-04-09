28ddd28126f6283ba634e31dba62b8235276f32d
BigMoistLochu <krok.konrad1@gmail.com>
2024-04-09 09:26:57 +0200

    add project to remote repo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //task 1: zrob klase ktora bedzie miala funkcje ktora jako parametr przyjmuje stringa w postaci linku do strony
+        //task 2:
+
+        System.out.println("Hello world!");
+    }
+}
