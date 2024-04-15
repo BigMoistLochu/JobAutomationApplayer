14447273b7efa4c504689ab2b9fb06819f314432
BigMoistLochu <krok.konrad1@gmail.com>
2024-04-15 23:52:42 +0200

    add task for scheduler, for ever 10 minute, web scrawwler will scrape the offert from pracujpl

--- src/main/java/org/example/Scheduler.java
+++ src/main/java/org/example/Scheduler.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.models.JobOffertDto;
+
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -8,7 +11,16 @@ public final class Scheduler {
     private static Scheduler INSTANCE;
 
     private final Timer scheduler = new Timer();
-    private Scheduler() {}
+    private Scheduler() {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                PracujPlJobCrawler pracujPlJobCrawler = new PracujPlJobCrawler();
+                List<JobOffertDto> offertDtos =  pracujPlJobCrawler.setPage("https://it.pracuj.pl/praca?sc=0&itth=38").scrapJobOffers();
+            }
+        };
+        scheduler.schedule(timerTask,0,10*60*1000);
+    }
 
     public synchronized static Scheduler getInstance() {
         if(INSTANCE == null) {
